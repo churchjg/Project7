0ad79b6d2442e1ad117d0726c50aaf6903461aa8
Jon <churchjg283@gmail.com>
2020-11-16 18:08:30 -0500

    small changes

--- front-end/src/Components/Forms/SubmissionForm.js
+++ front-end/src/Components/Forms/SubmissionForm.js
@@ -5,19 +5,19 @@ import RecipeForm from './RecipeForm'
 
 
 export class SubmissionForm extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             newName: ""
-            ,success: ""
-            ,type: ""
-            ,method: this.props.method
+            , success: ""
+            , type: ""
+            , method: this.props.method
         }
         this.baseState = {
             newName: ""
-            ,success: ""
-            ,type: ""
-            ,method: this.state.method
+            , success: ""
+            , type: ""
+            , method: this.state.method
         }
     }
 
@@ -25,32 +25,32 @@ export class SubmissionForm extends Component {
         console.log("success fired")
         this.setState({
             success: success.success
-            ,newName: success.newName
+            , newName: success.newName
         })
     }
 
     successDisplay = () => {
-        if (this.props.method === "POST"){
-            return <div style={{margin: 20}}>
-            <h3>Success! The new item name is {this.state.newName}</h3>
-            <Button style={{marginTop: 20, backgroundColor: "#F5F5F5", color: "#333333", border: "none"}} type="button" onClick={this.resetPage}>Submit Another New Recipe</Button>
+        if (this.props.method === "POST") {
+            return <div style={{ margin: 20 }}>
+                <h3>Success! The new item name is {this.state.newName}</h3>
+                <Button style={{ marginTop: 20, backgroundColor: "#F5F5F5", color: "#333333", border: "none" }} type="button" onClick={this.resetPage}>Submit Another New Recipe</Button>
             </div>
         }
-        if (this.props.method === "PUT"){
-            return <div style={{margin: 20}}>
-            <h3>Success! Recipe {this.state.newName} has been updated.</h3>
-            <Button style={{marginTop: 20, backgroundColor: "#F5F5F5", color: "#333333", border: "none"}} type="button" onClick={this.resetPage}>Submit Another Update</Button>
+        if (this.props.method === "PUT") {
+            return <div style={{ margin: 20 }}>
+                <h3>Success! Recipe {this.state.newName} has been updated.</h3>
+                <Button style={{ marginTop: 20, backgroundColor: "#F5F5F5", color: "#333333", border: "none" }} type="button" onClick={this.resetPage}>Submit Another Update</Button>
             </div>
         }
-        if (this.props.method === "DELETE"){
-            return <div style={{margin: 20}}>
-            <h3>Success! Recipe {this.state.newName} has been deleted.</h3>
-            <Button style={{marginTop: 20, backgroundColor: "#F5F5F5", color: "#333333", border: "none"}} type="button" onClick={this.resetPage}>Delete Another Recipe</Button>
+        if (this.props.method === "DELETE") {
+            return <div style={{ margin: 20 }}>
+                <h3>Success! Recipe {this.state.newName} has been deleted.</h3>
+                <Button style={{ marginTop: 20, backgroundColor: "#F5F5F5", color: "#333333", border: "none" }} type="button" onClick={this.resetPage}>Delete Another Recipe</Button>
             </div>
         }
     }
 
-    setFormType = e =>{
+    setFormType = e => {
         e.preventDefault()
         this.setState({
             type: e.target.value
@@ -62,43 +62,45 @@ export class SubmissionForm extends Component {
         this.setState(this.baseState)
     }
 
-    formType = () =>{
-        if (this.props.method === "POST"){
-            return <h3>Choose "Edit Recipes" in the Dropdown Menu to add a new Recipe</h3>
+    formType = () => {
+        if (this.props.method === "POST") {
+            return <h3>Choose "Add" in the Dropdown Menu to add a new Recipe</h3>
         }
-        if (this.props.method === "PUT"){
-            return <h3>Click "Edit Recipes" in the Dropdown Menu to update a Recipe</h3>
+        if (this.props.method === "PUT") {
+            return <h3>Click "Update" in the Dropdown Menu to update a Recipe</h3>
         }
-        if (this.props.method === "DELETE"){
-            return <h3>Select "Edit Recipes" in the Dropdown Menu to delete a Recipe</h3>
+        if (this.props.method === "DELETE") {
+            return <h3>Select "Delete" in the Dropdown Menu to delete a Recipe</h3>
         }
-        
+
     }
 
     displayForm = () => {
 
-        if (this.state.type === "foodInfo"){
-            return <RecipeForm type={this.state.type}  method={this.state.method} onSuccess={this.onSuccess} />
+        if (this.state.type === "foodInfo") {
+            return <RecipeForm type={this.state.type} method={this.state.method} onSuccess={this.onSuccess} />
         }
 
-        else {return null}
+        else { return null }
     }
 
     render() {
-        if (this.state.success !== true){
-        return (<div style={{ width: "60%", marginLeft: 20}}>
-            {this.formType()}
-            <Form style={{marginBottom: 10}}>
-                <Form.Control size="sm" as="select" id="basic-nav-dropdown" name="foodInfo" onChange={this.setFormType}>
-                    <option>Select</option>
-                    <option value="foodInfo">Edit Recipes</option>
-                </Form.Control> 
-            </Form>
-            {this.displayForm()} 
+        if (this.state.success !== true) {
+            return (<div style={{ width: "60%", marginLeft: 20 }}>
+                {this.formType()}
+                <Form style={{ marginBottom: 10 }}>
+                    <Form.Control size="sm" as="select" id="basic-nav-dropdown" name="foodInfo" onChange={this.setFormType}>
+                        <option>Select</option>
+                        <option value="foodInfo">Add</option>
+                        <option value="foodInfo">Update</option>
+                        <option value="foodInfo">Delete</option>
+                    </Form.Control>
+                </Form>
+                {this.displayForm()}
             </div>
-        )
+            )
         }
-        else {return this.successDisplay()}
+        else { return this.successDisplay() }
     }
 }
 
